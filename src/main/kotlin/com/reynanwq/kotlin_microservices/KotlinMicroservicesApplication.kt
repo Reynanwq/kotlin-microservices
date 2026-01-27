4c23f906bd1d8794c5c7dadcc06c6f080c64cd82
reynanwq <reynanwq@gmail.com>
2026-01-27 07:14:14 -0300

    first commit

---
+++ src/main/kotlin/com/reynanwq/kotlin_microservices/KotlinMicroservicesApplication.kt
@@ -0,0 +1,11 @@
+package com.reynanwq.kotlin_microservices
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinMicroservicesApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinMicroservicesApplication>(*args)
+}
---
+++ src/test/kotlin/com/reynanwq/kotlin_microservices/KotlinMicroservicesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.reynanwq.kotlin_microservices
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinMicroservicesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
